1b0cb6a1678a14a32afabbfdc404c8766ad85819
omotosho olushola mary <101481226+shollybee@users.noreply.github.com>
2022-05-03 04:25:24 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,73 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
 /**
- * check_num - check - string there are digit
- * @str: array str
+ * digit - checks the string in argv is a number
  *
- * Return; Always 0 (Success)
+ * Description: checks the string in argv is a number
+ *
+ * @s: pointer
+ * Return: 1 if true -1 if false
  */
-
-int check_num(char *str)
-
+int digit(char *s)
 {
-/*Declaring variables*/
-unsigned int count;
+	int i;
+	int x = 0;
 
-count = 0;
-while (count < strlen(str)) /*count string*/
-
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
-}
-
-count++;
-}
-return (1);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			x = x * 10 + (s[i] - '0');
+		else
+			return (-1);
+	}
+	return (0);
 }
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main -  adds positive numbers
  *
- * return: Always 0 (Success)
+ * Description:  adds positive numbers
+ *
+ * @argc: integer
+ * @argv: pointer
+ * Return: 0 or 1 if wrong
  */
-
-int main(int argc, char *argv[])
-
+int main(int argc, char **argv)
 {
+	int i;
+	int y = 0;
 
-/*Declaring variables*/
-int count;
-int str_to_int;
-int sum = 0;
-
-count = 1;
-while (count < argc) /*Goes through the whole array*/
-{
-if(check_num(argv[count]))
-
-{
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-sum += str_to_int;
-}
-
-/*Condition if one of the number contains symbols that are not digits*/
-else
-{
-printf("Error\n");
-return (1);
-}
-
-count++;
-}
-
-printf("%d\n", sum); /*print sum*/
-
-return (0);
+	if (argc >= 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (digit(argv[i]))
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+			else
+				y += atoi(argv[i]);
+		}
+		printf("%d\n", y);
+	}
+	else
+	{
+		if (argc == 1)
+		{
+			printf("%d\n", 0);
+		}
+		if (argc == 2)
+			printf("%s\n", "Error");
+	}
+	return (0);
 }
