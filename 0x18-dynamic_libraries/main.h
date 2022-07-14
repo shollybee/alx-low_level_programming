3e68e20e8bd691f523091c06ea58f054d9e7feef
omotosho olushola mary <101481226+shollybee@users.noreply.github.com>
2022-07-15 00:09:19 +0100

    Update and rename holberton.h to main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef main_h
+#define main_h
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H */
+#endif /* main_h */
